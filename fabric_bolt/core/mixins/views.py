f1e92d3cbf91cfb7a484ec11439a3a7fc1d60ec4
dieg.pascual <diego.pascua@scytl.com>
2014-09-04 13:26:32 +0200

    fixed infinite redirection loop when user is authenticated but don't has group permissions

--- fabric_bolt/core/mixins/views.py
+++ fabric_bolt/core/mixins/views.py
@@ -1,5 +1,7 @@
 
 from django.contrib import messages
+from django.contrib.auth.views import redirect_to_login
+from django.shortcuts import redirect
 
 from braces.views import GroupRequiredMixin
 
@@ -16,6 +18,23 @@ class MultipleGroupRequiredMixin(GroupRequiredMixin):
 
         is_member = group in user_groups
         if not is_member:
-            messages.add_message(self.request, messages.ERROR, 'You do not have sufficient permissions to do that.')
+            string_msg = 'You do not have sufficient permissions to do that.'
+            messages.add_message(self.request, messages.ERROR, string_msg)
 
         return is_member
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+
+        if self.request.user.is_authenticated():
+            if not self.check_membership(self.get_group_required()):
+                referer = request.META.get('HTTP_REFERER', '/')
+                return redirect(referer)
+        else:
+            return redirect_to_login(
+                request.get_full_path(),
+                self.get_login_url(),
+                self.get_redirect_field_name())
+
+        return super(GroupRequiredMixin, self).dispatch(
+            request, *args, **kwargs)
